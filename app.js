bcdaf642d46fa89d77e5b5f9db25596ccebaec00
Chris-Skipsey1 <K1915275@kingston.ac.uk>
2022-10-19 19:16:35 +0100

    User Story of medicines + prescriptions

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const app = new express();
 // Configure middleware ----------------------------------
 
 // Controllers ----------------------------------
-const modulesController = async (req, res) => {
+const medicinesController = async (req, res) => {
     //Build SQL
     const table = 'medicines';
     const fields = ['MedicineID','MedicineName', 'MedicineDescription', 'MedicineTakenDate'];
-    const extendedTable = `${table} LEFT JOIN prescriptions ON Prescriptions.PrescriptionMedicineID = Medicines.MedicineID`;
-    const extendedFields = `${fields}, medicines.MedicineID, medicines.MedicineName, Prescriptions.PrescriptionDosage`;
+    const extendedTable = `${table}` //LEFT JOIN prescriptions ON Prescriptions.PrescriptionMedicineID = Medicines.MedicineID`;
+    const extendedFields = `${fields}`//, medicines.MedicineID, medicines.MedicineName, Prescriptions.PrescriptionDosage`;
     const sql = `SELECT ${extendedFields} FROM ${extendedTable}`;
     // Execute query
     let isSuccess = false;
@@ -36,8 +36,40 @@ isSuccess
     : res.status(400).json({ message });
 };
 
+const medicinesPrescriptionController = async (req, res) => {
+    const id = req.params.id;
+    //Build SQL
+    const table = 'medicines';
+    const whereField = 'prescriptions.PrescriptionClientID';
+    const fields = ['MedicineID','MedicineName', 'MedicineDescription', 'MedicineTakenDate'];
+    const extendedTable = `prescriptions LEFT JOIN ${table} ON prescriptions.PrescriptionMedicineID = medicines.MedicineID`;
+    const extendedFields = `${fields}, medicines.MedicineID, medicines.MedicineName, prescriptions.PrescriptionDosage`;
+    const sql = `SELECT ${extendedFields} FROM ${extendedTable} WHERE ${whereField}=${id}`;
+    console.log(sql);
+    // Execute query
+    let isSuccess = false;  
+    let message = "";
+    let result = null;
+    try {
+        [result] = await database.query(sql);
+        if (result.length === 0) message = 'No record(s) found';
+        else {
+            isSuccess = true;
+            message = 'Record(s) successfully recovered';
+        }
+    }
+    catch (error) { 
+        message = `Failed to execute query: ${error.message}`;
+    }
+//Responses
+isSuccess
+    ? res.status(200).json(result)
+    : res.status(400).json({ message });
+};
+
 // Endpoints ----------------------------------
-app.get('/api/medicines', modulesController);
+app.get('/api/medicines', medicinesController);
+app.get('/api/medicines/clients/:id', medicinesPrescriptionController);
 
 // Start server ----------------------------------
 const PORT = process.env.PORT || 5000;
